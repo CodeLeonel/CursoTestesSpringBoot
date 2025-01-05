e656bb9d5c6a3445b4c71eb127698c378a915add
Hugo Leonel <hugoleonel.code@gmail.com>
2025-01-04 22:45:11 -0300

    Mockito com Springboot

--- src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -2,8 +2,10 @@ package com.example.swplanetapi.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 
@@ -13,8 +15,13 @@ public class PlanetServiceTest {
     @Autowired
     private PlanetService planetService;
 
+    @MockitoBean
+    private PlanetRepository planetRepository;
+
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
+
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
 
         Planet sut = planetService.create(PLANET);
         
