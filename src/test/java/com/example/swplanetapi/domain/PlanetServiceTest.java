54e504077846a64a4635b1973e661fa776af38cc
Hugo Leonel <hugoleonel.code@gmail.com>
2025-01-05 21:34:05 -0300

    Teste unitário de exceção ao inserir planeta

--- src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.swplanetapi.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +9,7 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 
 
@@ -28,10 +30,19 @@ public class PlanetServiceTest {
     public void createPlanet_WithValidData_ReturnsPlanet() {
 
         when(planetRepository.save(PLANET)).thenReturn(PLANET);
-
+        
         Planet sut = planetService.create(PLANET);
         
         assertThat(sut).isEqualTo(PLANET);
+
+    }
+    
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
 
     }
 
